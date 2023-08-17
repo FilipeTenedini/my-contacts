05fb201a531feca314580b93a124ff8c110a6940
filipetenedini <tenedinifilipe@gmail.com>
2023-08-17 11:50:36 -0300

    feat: Create button, input and select standard component

---
+++ src/components/Select.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  height: 52px;
+  padding: 0 16px;
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  background-color: #FFF;
+  border: 2px solid #FFF;
+  outline: none;
+  font-size: 16px;
+  transition: .2s ease-in;
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+  }
+`;
