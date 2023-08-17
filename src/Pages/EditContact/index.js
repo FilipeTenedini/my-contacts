6e1715fd4fe017038eac0b6fa74b53e0b66813f5
filipetenedini <tenedinifilipe@gmail.com>
2023-08-17 12:17:02 -0300

    feat: create contact form

--- src/Pages/EditContact/index.js
+++ src/Pages/EditContact/index.js
@@ -1,9 +1,15 @@
 import PageHeader from '../../components/PageHeader';
+import ContactForm from '../../components/ContactsForm';
 
 export default function EditContact() {
   return (
-    <PageHeader
-      title="Editar Filipe Tenedini"
-    />
+    <>
+      <PageHeader
+        title="Editar Filipe Tenedini"
+      />
+
+      <ContactForm buttonLabel="Salvar alterações" />
+    </>
+
   );
 }
--- src/Pages/NewContact/index.js
+++ src/Pages/NewContact/index.js
@@ -1,7 +1,5 @@
 import PageHeader from '../../components/PageHeader';
-import Input from '../../components/Input';
-import Select from '../../components/Select';
-import Button from '../../components/Button';
+import ContactForm from '../../components/ContactsForm';
 
 export default function NewContact() {
   return (
@@ -9,18 +7,8 @@ export default function NewContact() {
       <PageHeader
         title="Novo Contato"
       />
-      <Input typpe="text" placeholder="Nome" />
-      <Select>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-      </Select>
-      <Button type="button">
-        Salvar alterações
-      </Button>
-      <Button type="button" disabled>
-        Salvar alterações
-      </Button>
+
+      <ContactForm buttonLabel="Cadastrar" />
     </>
   );
 }
