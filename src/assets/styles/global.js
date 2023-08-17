77c89abed2d0121358e5bf1a18e4b2adc13b0f68
filipetenedini <tenedinifilipe@gmail.com>
2023-08-17 10:22:22 -0300

    feat: ContactsList body

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -11,6 +11,8 @@ export default createGlobalStyle`
   body {
     font-size: 16px;
     background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.gray[900]};
+
   }
 
   button {
